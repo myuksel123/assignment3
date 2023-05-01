cfc0e63e97fa518f9aa1ac46a09161924448e429
Meryem Yuksel <myuksel@ufl.edu>
2023-05-01 15:13:39 -0400

    fixed some errors and put some librosa load

--- audioprocessing.py
+++ audioprocessing.py
@@ -1,13 +1,16 @@
 import pandas as panda;
 import random;
-from scipy.io import wavefile;
+import librosa;
+#from scipy.io import wavefile;
 from pathlib import Path;
 
+#sorting them into train and test datasets
+#randomly put into each
 
-pathToSad = ("/data_folder/sad")
-pathToHappy = ("/data_folder/happy")
-pathToAngry = ("/data_folder/angry")
-pathToFear = ("/data_folder/fear")
+pathToSad = Path("./data_folder/sad")
+pathToHappy = Path("./data_folder/happy")
+pathToAngry = Path("./data_folder/angry")
+pathToFear = Path("./data_folder/fear")
 
 sadNamesTest = []
 sadNamesTrain = []
@@ -23,28 +26,35 @@ fearNamesTrain=[]
 
 for sadAudio in pathToSad.glob("*.wav"):
     x = random.randint(1,100)
-    if (x<=30 and sadNamesTest.__sizeof__<=30):
-        sadNamesTest.append(sadAudio)
+    if (x<=30 and len(sadNamesTest)<=30):
+        sadNamesTest.append(str(sadAudio))
     else:
-        sadNamesTrain.append(sadAudio)
+        sadNamesTrain.append(str(sadAudio))
 
 for fearAudio in pathToFear.glob("*.wav"):
     x = random.randint(1,100)
-    if (x<=30 and fearNamesTest.__sizeof__<=30):
-        fearNamesTest.append(fearAudio)
+    if (x<=30 and len(fearNamesTest)<=30):
+        fearNamesTest.append(str(fearAudio))
     else:
-        fearNamesTrain.append(fearAudio)
+        fearNamesTrain.append(str(fearAudio))
 
 for happyAudio in pathToHappy.glob("*.wav"):
     x = random.randint(1,100)
-    if (x<=30 and happyNamesTest.__sizeof__<=30):
-        happyNamesTest.append(happyAudio)
+    if (x<=30 and len(happyNamesTest)<=30):
+        happyNamesTest.append(str(happyAudio))
     else:
-        happyNamesTrain.append(happyAudio)
+        happyNamesTrain.append(str(happyAudio))
 
 for angryAudio in pathToAngry.glob("*.wav"):
     x = random.randint(1,100)
-    if (x<=30 and angryNamesTest.__sizeof__<=30):
-        angryNamesTest.append(angryAudio)
+    if (x<=30 and len(angryNamesTest)<=30):
+        angryNamesTest.append(str(angryAudio))
     else:
-        angryNamesTrain.append(angryAudio)
+        angryNamesTrain.append(str(angryAudio))
+
+#Exploratory Data Analysis
+
+#Using librosa
+sad, sad2 = librosa.load(sadNamesTest[0])
+print(type(sad))
+print(type(sad2))
